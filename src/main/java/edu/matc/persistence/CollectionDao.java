11c5f32642ceb80d93dee768a320baf8c45f154c
Eric <monforte@madisoncollege.edu>
2017-02-12 18:50:40 -0600

    collection dao done

---
+++ src/main/java/edu/matc/persistence/CollectionDao.java
@@ -0,0 +1,75 @@
+package edu.matc.persistence;
+
+import edu.matc.entity.Collection;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by student on 2/12/17.
+ */
+public class CollectionDao {
+
+    /** Return a list of all users' collection
+     *
+     * @return All collections
+     */
+    public List<Collection> getAll() throws Exception{
+        List<Collection> collections = new ArrayList<Collection>();
+
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        collections = session.createCriteria(Collection.class).list();
+
+        session.close();
+        return collections;
+    }
+
+    /**
+     * Add collection int.
+     *
+     * @param collection the collection
+     * @return the id of the collection
+     */
+    public int addCollection(Collection collection) throws Exception {
+        int id = 0;
+
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        id = (int) session.save(collection);
+        transaction.commit();
+
+        session.close();
+        return id;
+
+    }
+
+    public void deleteCollection(int id) throws Exception {
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Collection collection = new Collection();
+        collection.setCollectionId(id);
+
+        session.delete(collection);
+        transaction.commit();
+
+        session.close();
+
+    }
+
+    public void updateCollection(Collection collection) throws Exception {
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.update(collection);
+        transaction.commit();
+
+        session.close();
+    }
+
+
+
+}
